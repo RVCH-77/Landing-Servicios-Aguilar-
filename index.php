601bae07c3d4f2e18b20da895c9bc6182e438698
Christopher Isaac Rivera Valderrama <christoferriveravalderrama@gmail.com>
2026-09-13 00:56:13 -0600

    FIX: Se añadio la barra de navegacion unicacmente el cascaron donde establecieron unicamente cuatro puntos son las principales puntos para la pagina

--- index.php
+++ index.php
@@ -8,6 +8,16 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 
 </head>
+<header>
+    <nav class="nav">
+        <a class= "nav-link" arial-current= "page" href= "#"> Inicio
+        <a class= "nav-link" arial-current= "page" href= "#"> Nosotros
+        <a class= "nav-link" arial-current= "page" href= "#"> Servicios
+        <a class= "nav-link" arial-current= "page " href= "#"> Contacto
+</nav>
+</header>
+
+
 <body>
     
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
